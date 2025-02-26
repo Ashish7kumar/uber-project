5bbae64c1cc9f0f2758ba3b9b8f83e8074fcac74
Ashish7kumar <ashish11to12@gmail.com>
2025-02-26 11:47:06 +0530

    well starting a uber project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const dotenv=require('dotenv');
+dotenv.config();
+const http=require('http');
+const app=require('./app');
+const port=process.env.PORT;
+const server=http.createServer(app);
+server.listen(3000,()=>{
+   console.log('server is runing on port'+port);
+    
+});
